57daae3ecf182d53481cdfc6ee9196533e1e91bd
Vitalvarma <157305632+Vitalvarma@users.noreply.github.com>
2025-02-25 14:41:34 +0530

    Add files via upload

---
+++ 6/6b/server.js
@@ -0,0 +1,12 @@
+function popupAlert(){
+    alert("popup Alert");
+}
+function displayDoc(){
+    document.getElementById("doc").innerHTML="displaying documentation";
+}
+function logToConsole(){
+    console.log("Hello (Log to console)");
+}
+function innerHtml(){
+    document.getElementById("doc").innerHTML="updated through the innerHtml";
+}
